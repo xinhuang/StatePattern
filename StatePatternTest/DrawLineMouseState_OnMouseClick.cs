9ac3c4381d0c0ce6ac44b69057b65da94c42bf39
Patz Huang <walkaniceday@gmail.com>
2013-01-22 13:52:25 +0800

    Clean up.

--- StatePatternTest/DrawLineMouseState_OnMouseClick.cs
+++ StatePatternTest/DrawLineMouseState_OnMouseClick.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using StatePattern;
@@ -9,19 +8,24 @@ namespace StatePatternTest
     [TestClass]
     public class DrawLineMouseState_OnMouseClick
     {
+        private readonly DrawLineMouseState _sut = new DrawLineMouseState();
+        private readonly Mock<IMouse> _mockMouse = new Mock<IMouse>();
+        private readonly Mock<IDrawPad> _mockPad = new Mock<IDrawPad>();
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _mockMouse.SetupGet(o => o.DrawPad).Returns(_mockPad.Object);
+        }
+
         [TestMethod]
         public void given_2_click_should_add_a_line_object_to_draw_pad_and_reset_mouse()
         {
-            var sut = new DrawLineMouseState();
-            var mockMouse = new Mock<IMouse>();
-            var mockPad = new Mock<IDrawPad>();
-            mockMouse.SetupGet(o => o.DrawPad).Returns(mockPad.Object);
+            _sut.OnMouseClick(_mockMouse.Object, new Point(1, 1));
+            _sut.OnMouseClick(_mockMouse.Object, new Point(2, 2));
 
-            sut.OnMouseClick(mockMouse.Object, new Point(1, 1));
-            sut.OnMouseClick(mockMouse.Object, new Point(2, 2));
-
-            mockMouse.Verify(o => o.Reset(), Times.Once());
-            mockPad.Verify(o => o.Add(It.IsAny<Shape>()), Times.Once());
+            _mockMouse.Verify(o => o.Reset(), Times.Once());
+            _mockPad.Verify(o => o.Add(It.IsAny<Shape>()), Times.Once());
         }
     }
 }
